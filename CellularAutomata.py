700538c07ff1f499a04b19b4f2e08d1242773235
Oscar015 <65154615+Oscar015@users.noreply.github.com>
2021-11-20 16:09:25 +0100

    Update CellularAutomata.py

--- CellularAutomata.py
+++ CellularAutomata.py
@@ -179,9 +179,7 @@ class GameOfLife(CellularAutomata):
 
 class SmoothNoise(CellularAutomata):
     def function(self, neighbours, i, j):
-        if neighbours >= 6:
+        if neighbours >= 5:
             return 1
-        elif neighbours <= 3:
+        elif neighbours < 5:
             return 0
-        else:
-            return self.grid[i, j]
