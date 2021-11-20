7233d3c3c185cd64f016c8589419419acbe3369c
Oscar015 <65154615+Oscar015@users.noreply.github.com>
2021-11-20 15:35:43 +0100

    Generalize de main class
    This generalization allows to create subclases of Cellular Automata with others rules

--- ConwayLifeGame.py
+++ ConwayLifeGame.py
@@ -10,7 +10,7 @@ from matplotlib.patches import Rectangle
 from matplotlib.animation import FuncAnimation
 
 
-class GameOfLife:
+class CellularAutomata:
 
     def __init__(self, seed):
         """
@@ -26,24 +26,33 @@ class GameOfLife:
         """
         self.seed = seed
         self.grid = seed
+        # La región de juego será un cuadrado, para evitar errores se tomará
+        # el menor de los tamaños horizontal y vertical.
+        self.N = np.min(seed.shape)
+
+    def reset(self):
+        self.grid = self.seed
 
     def plotSeed(self, figArgs={'dpi': 150, 'figsize': (4, 4)}):
-        rects = np.zeros((N, N), dtype=Rectangle)
+        rects = np.zeros((self.N, self.N), dtype=Rectangle)
 
         fig, ax = plt.subplots(**figArgs)
         ax.axis('equal')
-        ax.set_xlim(0, N)
-        ax.set_ylim(0, N)
+        ax.set_xlim(0, self.N)
+        ax.set_ylim(0, self.N)
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
 
         # Rellenamos el array con rectangulos del color adecuado a la célula
-        for i in range(N):
-            for j in range(N):
+        for i in range(self.N):
+            for j in range(self.N):
                 rects[i, j] = Rectangle(
-                    [i, N-j-1], 1, 1, color=self.setColor(j, i, self.seed))
+                    [i, self.N-j-1], 1, 1, color=self.setColor(j, i, self.seed))
                 ax.add_artist(rects[i, j])
         plt.show()
+
+    def function(self, neighbours, i, j):
+        return self.grid[i, j]
 
     def update(self):
         """
@@ -51,67 +60,63 @@ class GameOfLife:
 
         """
         # Creamos una matriz y forzamos que en los bordes valga 0
-        N, M = self.grid.shape
         newGrid = self.grid.copy()
         newGrid[0, :] = 0
-        newGrid[N-1, :] = 0
+        newGrid[self.N-1, :] = 0
 
         newGrid[:, 0] = 0
-        newGrid[:, M-1] = 0
+        newGrid[:, self.N-1] = 0
 
         # Recorremos todas las celulas
-        for i in range(1, N-1):
-            for j in range(1, M-1):
+        for i in range(1, self.N-1):
+            for j in range(1, self.N-1):
                 neighbours = 0
                 # Miramos los vecinos de la célula y contamos als vivas
                 for x in range(-1, 2):
                     for y in range(-1, 2):
                         if not x == y == 0:
                             neighbours += self.grid[i+x, j+y]
-                # Una célula muerta con 3 vecinos nace
-                if self.grid[i, j] == 0 and neighbours == 3:
-                    newGrid[i, j] = 1
-                # Una célula muerta con 2 o 3 vecinos sigue viva
-                elif self.grid[i, j] == 1 and 2 <= neighbours <= 3:
-                    newGrid[i, j] = 1
-                # En otro caso muere
-                else:
-                    newGrid[i, j] = 0
+                newGrid[i, j] = self.function(neighbours, i, j)
+
         self.grid = newGrid
 
-    def animation(self, figArgs={'dpi': 150, 'figsize': (4, 4)}):
+    def animation(self, figArgs={'dpi': 150, 'figsize': (4, 4)}, animArgs={'frames': 1, 'interval': 200}):
         """
         Crea la animación del Juego de la vida
 
         """
+
         # Creamos un array de rectangulos vacio que representarán a las células
-        self.rects = np.zeros((N, N), dtype=Rectangle)
+        self.rects = np.zeros((self.N, self.N), dtype=Rectangle)
 
         self.fig, self.ax = plt.subplots(**figArgs)
         self.ax.axis('equal')
-        self.ax.set_xlim(0, N)
-        self.ax.set_ylim(0, N)
+        self.ax.set_xlim(0, self.N)
+        self.ax.set_ylim(0, self.N)
         self.ax.xaxis.set_visible(False)
         self.ax.yaxis.set_visible(False)
 
         # Rellenamos el array con rectangulos del color adecuado a la célula
-        for j in range(N):
-            for i in range(N):
+        for j in range(self.N):
+            for i in range(self.N):
                 self.rects[i, j] = Rectangle(
-                    [i, N-j-1], 1, 1, color=self.setColor(j, i, self.grid))
+                    [i, self.N-j-1], 1, 1, color=self.setColor(j, i, self.grid))
                 self.ax.add_artist(self.rects[i, j])
+
+        def init():
+            self.reset()
 
         def animate(k):
             # Cambiamos los colores de los cuadrados
-            for j in range(N):
-                for i in range(N):
+            for i in range(self.N):
+                for j in range(self.N):
                     self.rects[i, j].set_color(self.setColor(j, i, self.grid))
             # Y actualizamos la simulación
             self.update()
             return self.rects,
 
         self.anim = FuncAnimation(
-            self.fig, animate, frames=30, interval=200)
+            self.fig, animate, **animArgs)
         return self.anim
 
     def setColor(self, i, j, grid):
@@ -137,44 +142,25 @@ class GameOfLife:
         else:
             return 'black'
 
-##########################################################################
+
+class GameOfLife(CellularAutomata):
+    def function(self, neighbours, i, j):
+        # Una célula muerta con 3 vecinos nace
+        if self.grid[i, j] == 0 and neighbours == 3:
+            return 1
+        # Una célula muerta con 2 o 3 vecinos sigue viva
+        elif self.grid[i, j] == 1 and 2 <= neighbours <= 3:
+            return 1
+        # En otro caso muere
+        else:
+            return 0
 
 
-if __name__ == '__main__':
-    N = 20
-
-    # seed = np.zeros((N, N), dtype=int)
-    # init = [N//3, N//3], [N//3, N//3+1], [N//3-1, N//3+2],\
-    #     [N//3+1, N // 3+2], [2*N//3, 2*N//3],\
-    #     [2*N//3, 2*N//3+1], [2*N//3, 2*N//3+2], [2*N//3+2, 2*N//3],\
-    #     [2*N//3-1, 2*N//3]
-
-    # for index in init:
-    #     seed[index[1], index[0]] = 1
-
-    seed = np.array([
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-
-    GOL = GameOfLife(seed)
-    GOL.plotSeed(figArgs={'dpi': 150, 'figsize': (5, 5)})
-    anim = GOL.animation(figArgs={'dpi': 150, 'figsize': (5, 5)})
-    plt.show()
+class SmoothNoise(CellularAutomata):
+    def function(self, neighbours, i, j):
+        if neighbours >= 4:
+            return 1
+        elif neighbours <= 3:
+            return 0
+        else:
+            return self.grid[i, j]
