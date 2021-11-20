2d518d216180f3abe3933c563a1dd34d0a7d9e79
Oscar015 <65154615+Oscar015@users.noreply.github.com>
2021-11-20 16:08:14 +0100

    Update examples.py

--- examples.py
+++ examples.py
@@ -6,7 +6,7 @@ Created on Tue Nov 16 08:57:10 2021
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from ConwayLifeGame import GameOfLife, SmoothNoise
+from CellularAutomata import GameOfLife, SmoothNoise
 
 seed1 = np.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -30,19 +30,22 @@ seed1 = np.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
-seed2 = np.random.randint(0, 2, size=(50, 50))
+seed2 = np.random.randint(0, 2, size=(100, 100))
 
 figArgs = {'dpi': 100, 'figsize': (5, 5)}
-animArgs = {'frames': 100, 'interval': 200, 'init_func': None}
+animArgs1 = {'frames': 100, 'interval': 200, 'init_func': None}
+animArgs2 = {'frames': 5, 'interval': 200, 'repeat': False}
+
+
 GOL = GameOfLife(seed1)
-# GOL.plotSeed(figArgs)
-anim = GOL.animation(figArgs, animArgs)
+GOL.plotSeed(figArgs)
+GOL.update()
+GOL.plotState(figArgs)
+GOL.reset()
+anim = GOL.animation(figArgs, animArgs1)
 plt.show()
 
 
-animArgs = {'frames': 5, 'interval': 200, 'repeat': False}
 SN = SmoothNoise(seed2)
-# SN.plotSeed(figArgs)
-# anim = SN.animation(figArgs, animArgs)
-# plt.show()
-# anim.save('smoothNoise.gif')
+SN.plotSeed(figArgs)
+anim = SN.animation(figArgs, animArgs2, save=True, filePath='smoothNoise')
