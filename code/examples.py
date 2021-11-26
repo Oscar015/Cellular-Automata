a1e55547ddd504e5fc33c1c13a6404b1edefd352
Oscar015 <65154615+Oscar015@users.noreply.github.com>
2021-11-26 09:17:39 +0100

    Update examples.py

--- code/examples.py
+++ code/examples.py
@@ -12,20 +12,20 @@ seed = np.random.randint(0, 2, size=(100, 100))
 
 figArgs = {'dpi': 100, 'figsize': (5, 5)}
 animArgs1 = {'frames': 100, 'interval': 200, 'init_func': None}
-animArgs2 = {'frames': 10, 'interval': 200, 'repeat': False}
+animArgs2 = {'frames': 10, 'interval': 200, 'repeat': True}
 
 
-GOL = GameOfLife(seed)
-GOL.plotSeed(figArgs)
-GOL.update()
-GOL.plotState(figArgs)
-GOL.reset()
-anim = GOL.animation(figArgs, animArgs1, save=True,
-                     filePath='.\media\GameOfLive2')
-plt.show()
+# GOL = GameOfLife(seed)
+# GOL.plotSeed(figArgs)
+# GOL.update()
+# GOL.plotState(figArgs)
+# GOL.reset()
+# anim = GOL.animation(figArgs, animArgs1, save=True,
+#                      filePath='.\media\GameOfLive2')
+# plt.show()
 
 
 SN = SmoothNoise(seed)
-anim = SN.animation(figArgs, animArgs2, save=True,
+anim = SN.animation(figArgs, animArgs2, save=False,
                     filePath='.\media\smoothNoise2')
 plt.show()
